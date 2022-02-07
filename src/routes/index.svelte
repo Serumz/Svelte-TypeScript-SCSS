380e8afa81c28aac921930761ff221565cf8aa89
Serumz <65189904+Serumz@users.noreply.github.com>
2022-02-07 17:39:21 +0000

    Init

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+let value:number = 7;
+
+</script>
+
+<h1>Welcome to SvelteKit, heres your number {value}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style lang="scss">
+    p{
+      a{
+        color: red;
+      }
+    }
+</style>
